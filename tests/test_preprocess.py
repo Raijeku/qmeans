0ce9416d543166b9ad7243d8b35dfafde24e3976
Raijeku <raijeku@gmail.com>
2022-03-21 17:39:53 -0500

    Fixed testing errors

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,7 +1,7 @@
 import pytest
 from qmeans.qkmeans import *
 
+data = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
+
 def test_preprocess_probability():
-    data = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-    print(data)
-    assert 1 == 1
+    assert np.all(np.sum(data**2,axis=0) == np.ones(data.shape[1])*data.shape[0])
