5f4ca36b158a98966a50cca10ca327df049d5ada
Raijeku <raijeku@gmail.com>
2022-04-19 20:52:11 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup(
    description='Q-Means algorithm implementation using Qiskit compatible with Scikit-Learn.',
    license="Apache-2.0",
    long_description=long_description,
+   long_description_content_type='text/markdown',
    author='David Quiroga',
    author_email='raijeku@gmail.com',
    url="http://qmeans.readthedocs.io/",
    packages=['qmeans'],
-   install_requires=['wheel', 'numpy', 'pandas', 'qiskit', 'sklearn', 'pytest', 'hypothesis', 'sphinx', 'sphinx-rtd-theme', 'sphinxcontrib-napoleon'],
+   install_requires=['wheel', 'twine', 'setuptools', 'numpy', 'pandas', 'qiskit', 'sklearn', 'pytest', 'hypothesis', 'sphinx', 'sphinx-rtd-theme', 'sphinxcontrib-napoleon'],
 )
