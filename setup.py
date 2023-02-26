0c47e5c99d4dfb67f9bcc0e0fd4c460c34a8e961
David Quiroga <raijeku@gmail.com>
2023-02-26 02:02:58 -0500

    :card_index: Setting project up for new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", 'r', encoding="utf-8") as f:
 
 setup(
    name='qmeans',
-   version='0.1.1',
+   version='0.1.2',
    description='Q-Means algorithm implementation using Qiskit compatible with Scikit-Learn.',
    license="Apache-2.0",
    long_description=long_description,
