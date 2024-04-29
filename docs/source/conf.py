ca4b3e4def3d6006e683ffd81bc0b04fd9f11cbe
David Quiroga <raijeku@gmail.com>
2024-04-29 17:37:43 -0500

    :books: Changing documentation configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, David Quiroga'
 author = 'David Quiroga'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
@@ -49,7 +49,11 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+import sphinx_rtd_theme
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
